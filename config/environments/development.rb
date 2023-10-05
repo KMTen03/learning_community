b771aa18a6256cd17d9b37115ac43993093d66be
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2023-10-05 14:08:51 +0000

    間違って作ったマイグレーションファイル削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "c04ab231fe27438bb6e18a7769660f35.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
 end
