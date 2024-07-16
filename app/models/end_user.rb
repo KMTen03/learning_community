c9c0c9d0d15928e38b10b6cc995c51d7e23f425a
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-07-16 17:09:12 +0000

    コメントアウト追加

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -4,42 +4,45 @@ class EndUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :profile_image
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  
+  has_one_attached :profile_image
 
-
-
-  validates :introduce, length: { maximum: 200 }
-
+  validates :introduce, length: { maximum: 200 } # 自己紹介は200文字まで
 
 
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |guest|
-      guest.password = SecureRandom.urlsafe_base64
+      guest.password = SecureRandom.urlsafe_base64 #パスワードをランダム生成
       guest.name = "ゲスト"
-      guest.is_deleted = false
+      guest.is_deleted = false #ゲストの退会フラグはfalse
     end
   end
 
   def guest_user?
-    email == "guest@example.com"
+    email == "guest@example.com" #メアドが完全一致するとき
   end
     
   def self.ranking
     find(EndUserPost.group(:end_user_id).order('count(post_id) desc').limit(10).pluck(:end_user_id))
+    # UserPost.group(:user_id)#記事の番号が同じ物にグループを分ける
+    # order('count(post_id) desc')#多い順に並び変える
+    # limit(10)#最大10個
+    # pluck(:user_id)#そして最後に:user_idカラムのみを数字で取り出すように指定。
+    # user_idとpost_idがバラバラ、統一するならpost_idでは？
   end
     
   def get_profile_image(width, height)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/profile.jpeg')
+    unless profile_image.attached?　# プロフィールイメージが適用されていなければ用意してある画像を適用
+      file_path = Rails.root.join('app/assets/images/profile.jpeg') #app/assets/images/profile.jpeg このファイルから画像を参照
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg',content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
-  def active_for_authentication?
-    super && (is_deleted == false)
+  def active_for_authentication? #ユーザーが退会していないか判別
+    super && (is_deleted == false) #ユーザーのis_deletedがfalseならtrueを返す
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,16 +11,21 @@ class Post < ApplicationRecord
   validates :learning_content, presence: true
 
   
-  def save_tags(tags)
-    tag_list = tag.split(/[[:blank:]]+/)
-    current_tags = self.tags.pluck(:tag_name)
-    old_tags = current_tags - tag_list
-    new_tags = tags_list - current_tags
-    p current_tags
-    old_tags.each do |old|
+  def save_tags(tags) 
+    #タグを保存する
+    tag_list = tag.split(/[[:blank:]]+/) #分割して配列を作る
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    #unless~ は「タグが存在しているか」を確認している
+    old_tags = current_tags - tag_list 
+    #送信されてきたタグから現在存在するタグを除いたタグをnewとする
+    new_tags = tags_list - current_tags 
+    #送信されてきたタグから現在存在するタグを除いてoldtagとする
+    p current_tags # ???
+    
+    old_tags.each do |old| #古いタグを消す
       self.tags.delete Tag.find_by(tag_name: old)
     end
-    new_tags.each do |new|
+    new_tags.each do |new| #新しいタグを保存
       new_post_tag = Tag.find_or_create_by(name: new)
       self.tags << new_post_tag
     end
@@ -34,19 +39,20 @@ class Post < ApplicationRecord
     end
   end
   
-  def liked?(end_user)
+  def liked?(end_user) #いいね機能
     likes.where(end_user_id: end_user.id).exists?
   end
   
-  def self.search(keyword)
+  def self.search(keyword) #検索機能
     where("title LIKE ? or learning_time LIKE ? or learning_content LIKE ?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
+    # keywordのところに検索した文字列が適用され、そこからタイトルなのか、学習内容の検索なのか判定する
   end
   
-  def self.get_ranking(post)
+  def self.get_ranking(post) #なぜここに検索機能が
     find(Like.group(:post_id).order('count(post_id) desc').pluck(:post_id))
   end
   
-  def self.save_tags(post, tag_ids)
+  def self.save_tags(post, tag_ids) #タグ機能は今後改修予定
     tag_ids&.each do |tag_id|
       PostTag.create(post_id: post.id, tag_id: tag_id)
     end
